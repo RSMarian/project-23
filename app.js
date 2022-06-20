c0c05d63bc89e6bc2b64b5713003031a81168322
RSMarian <marianstefan1499@gmail.com>
2022-06-20 14:00:24 +0300

    added item-adding functionality

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const clearBtn = document.querySelector(".clear-btn");
 // edit option
 let editElement;
 let editFlag = false;
-let editId = "";
+let editID = "";
 // ****** EVENT LISTENERS **********
 // submit form
 form.addEventListener("submit", addItem);
@@ -20,9 +20,31 @@ function addItem(e) {
   e.preventDefault();
   const value = grocery.value;
   const id = new Date().getTime().toString();
-  if (value && !editFlag === false) {
-    console.log("add item to the list");
-  } else if (value && editFlag) {
+  if (value !== "" && !editFlag) {
+    const element = document.createElement("article");
+    // add class
+    element.classList.add("grocery-item");
+    // add id
+    let attr = document.createAttribute("data-id");
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.classList.add("grocery-item");
+    element.innerHTML = `<p class="title">${value}</p>
+    <div class="btn-container">
+      <button type="button" class="edit-btn">
+        <i class="fas fa-edit"></i>
+      </button>
+      <button type="button" class="delete-btn">
+        <i class="fas fa-trash"></i>
+      </button>
+    </div>`;
+    // append child
+    list.appendChild(element);
+    // display alert
+    displayAlert("item added to the list", "success");
+    // show container
+    container.classList.add("show-container");
+  } else if (value !== "" && editFlag) {
     console.log("editing");
   } else {
     displayAlert("please enter value", "danger");
